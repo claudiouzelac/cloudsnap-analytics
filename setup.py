0c6e5a6127389460a1ba7a240fa7c373198f73a8
Stewart Henderson <shenderson@mozilla.com>
2020-05-06 21:16:01 -0500

    shortcuts

--- setup.py
+++ setup.py
@@ -39,11 +39,22 @@ OPTIONS = {
 setup_requirements = ["py2app", "setuptools>=40.5.0", "cx-Freeze"]
 company_name = "Mozilla"
 product_name = "CloudSnap Analytics"
-bdist_msi_options = {
-    'upgrade_code': '{66620F3A-DC3A-11E2-B341-002219E9B01E}',
-    'add_to_path': False,
-    'initial_target_dir': r'[ProgramFilesFolder]\%s\%s' % (company_name, product_name),
-    }
+shortcut_table = [
+    ("DesktopShortcut",        # Shortcut
+     "DesktopFolder",          # Directory_
+     "DTI Playlist",           # Name
+     "TARGETDIR",              # Component_
+     "[TARGETDIR]app.exe",# Target
+     None,                     # Arguments
+     None,                     # Description
+     None,                     # Hotkey
+     None,                     # Icon
+     None,                     # IconIndex
+     None,                     # ShowCmd
+     'TARGETDIR'               # WkDir
+     )
+    ]
+msi_data = {"Shortcut": shortcut_table}
 build_exe_options  = {
     "packages": [
         "flask",
@@ -67,7 +78,9 @@ build_exe_options  = {
         "pkg-resources"
     ]
 }
-
+bdist_msi_options = {
+    'data': msi_data
+    }
 setup(
     name="cloudsnap-analytics",
     long_description=long_description,
