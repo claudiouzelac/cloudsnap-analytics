46e801ef8d19fe7f32215bdc8f09e622ceac6938
Stewart Henderson <shenderson@mozilla.com>
2020-05-06 21:03:20 -0500

    build_exe

--- setup.py
+++ setup.py
@@ -37,7 +37,11 @@ OPTIONS = {
 }
 
 setup_requirements = ["py2app", "setuptools>=40.5.0", "cx-Freeze"]
-
+bdist_msi_options = {
+    'upgrade_code': '{66620F3A-DC3A-11E2-B341-002219E9B01E}',
+    'add_to_path': False,
+    'initial_target_dir': r'[ProgramFilesFolder]\%s\%s' % (company_name, product_name),
+    }
 build_exe_options  = {
     "packages": [
         "flask",
@@ -74,6 +78,6 @@ setup(
         "Operating System :: OS Independent",
     ),
     install_requires=app_requirements,
-    options = {"build_exe": build_exe_options},
+    options = {"build_exe": build_exe_options,"bdist_msi": bdist_msi_options,},
     executables = [Executable('./app/app.py',base="Win32GUI")]
 )
